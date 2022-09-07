697ee46dcf8d63c9155b4b2e943e50c497d31f5c
wutsi <herve.tchepannou@wutsi.com>
2022-09-07 13:35:06 -0400

    WU-350: Pervent user to delete all pictures of a published product

--- src/main/kotlin/com/wutsi/application/store/endpoint/settings/product/picture/command/DeletePictureCommand.kt
+++ src/main/kotlin/com/wutsi/application/store/endpoint/settings/product/picture/command/DeletePictureCommand.kt
@@ -1,8 +1,12 @@
 package com.wutsi.application.store.endpoint.settings.product.picture.command
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.wutsi.application.store.endpoint.AbstractCommand
 import com.wutsi.ecommerce.catalog.WutsiCatalogApi
+import com.wutsi.ecommerce.catalog.error.ErrorURN
 import com.wutsi.flutter.sdui.Action
+import com.wutsi.platform.core.error.ErrorResponse
+import feign.FeignException
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -11,13 +15,23 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/commands/delete-picture")
 class DeletePictureCommand(
-    private val catalogApi: WutsiCatalogApi
+    private val catalogApi: WutsiCatalogApi,
+    private val objectMapper: ObjectMapper
 ) : AbstractCommand() {
     @PostMapping
     fun index(
         @RequestParam("picture-id") pictureId: Long
     ): Action {
-        catalogApi.deletePicture(pictureId)
-        return gotoPreviousScreen()
+        try {
+            catalogApi.deletePicture(pictureId)
+            return gotoPreviousScreen()
+        } catch (ex: FeignException) {
+            val response = objectMapper.readValue(ex.contentUTF8(), ErrorResponse::class.java)
+            return if (response.error.code == ErrorURN.PUBLISHED_PRODUCT_SHOULD_HAVE_IMAGE.urn) {
+                showError(getText("error.product.PUBLISHED_PRODUCT_SHOULD_HAVE_IMAGE"))
+            } else {
+                showError(getText("error.unexpected"))
+            }
+        }
     }
 }
--- src/test/kotlin/com/wutsi/application/store/endpoint/settings/product/picture/command/DeletePictureCommandTest.kt
+++ src/test/kotlin/com/wutsi/application/store/endpoint/settings/product/picture/command/DeletePictureCommandTest.kt
@@ -1,7 +1,11 @@
 package com.wutsi.application.store.endpoint.settings.product.picture.command
 
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.doThrow
 import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
 import com.wutsi.application.store.endpoint.AbstractEndpointTest
+import com.wutsi.ecommerce.catalog.error.ErrorURN
 import com.wutsi.flutter.sdui.Action
 import com.wutsi.flutter.sdui.enums.ActionType
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -28,5 +32,28 @@ internal class DeletePictureCommandTest : AbstractEndpointTest() {
         val action = response.body!!
         assertEquals(ActionType.Route, action.type)
         assertEquals("route:/..", action.url)
+    }
+
+    @Test
+    fun `delete all image from published products`() {
+        // GIVEN
+        val ex = createFeignException(ErrorURN.PUBLISHED_PRODUCT_SHOULD_HAVE_IMAGE.urn)
+        doThrow(ex).whenever(catalogApi).deletePicture(any())
+
+        // WHEN
+        val url = "http://localhost:$port/commands/delete-picture?picture-id=111"
+        val response = rest.postForEntity(url, null, Action::class.java)
+
+        // THEN
+        assertEquals(200, response.statusCodeValue)
+
+        verify(catalogApi).deletePicture(111)
+
+        val action = response.body!!
+        assertEquals(ActionType.Prompt, action.type)
+        assertEquals(
+            getText("error.product.PUBLISHED_PRODUCT_SHOULD_HAVE_IMAGE"),
+            action.prompt?.attributes?.get("message")
+        )
     }
 }
